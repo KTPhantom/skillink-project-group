1f732f74fe2fc6506e3ce07d38d0977d86bcd6f4
KryptoVader <divyansh.shekhar2004@gmail.com>
2024-11-20 21:35:02 +0530

    Backend integration

---
+++ app.js
@@ -0,0 +1,4 @@
+const Express = require("express");
+const PORT = 3000;
+
+app.use(Express.static('Public'));
--- config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    googleClientId: '485425670262-38doqu74r3520v5sjip2o1julcp40i2r.apps.googleusercontent.com', // Replace with your actual client ID
-    apiBaseUrl: 'http://localhost:3000' // Your backend API URL
-    }
